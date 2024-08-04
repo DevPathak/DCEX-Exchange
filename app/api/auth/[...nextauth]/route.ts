755fe51bcec4c9aff68564fe1349802c07840373
devesh.pathak <devesh.pathak@saarathi.ai>
2024-08-04 17:17:49 +0530

    removed junk

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,7 @@
-import prisma from "@/app/db";
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { Keypair } from "@solana/web3.js";
 import { authConfig } from "@/app/lib/auth";
 
 const handler = NextAuth(authConfig);
+console.log("Handler", handler);
 
 export { handler as GET, handler as POST };
